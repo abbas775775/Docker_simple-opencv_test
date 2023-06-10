6058d7dc2a79e130a81c04e245097436360bd620
amin asghari soureh <61617353+abbas775775@users.noreply.github.com>
2023-06-10 20:15:52 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -81,5 +81,5 @@ async def predict(files: UploadFile = File(...)):
 
 # Start the app in normal deployment
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
 
